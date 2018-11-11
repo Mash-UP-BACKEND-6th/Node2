7dd59a57443f97b8596a9af9c9fcd71ccc7c8004
Yuni-Q <carnotcycle2@naver.com>
2018-11-11 14:36:08 +0900

    Add 20181011_base

---
+++ 20181110_yun.js
@@ -0,0 +1,45 @@
+
+const fetch = require('node-fetch');
+const { parseString } = require('xml2js');
+
+let busStopNumbers = [];
+let busStopName= '홍대입구역';
+busStopName = encodeURI(busStopName);
+
+const busStopKey = 'ZToJL71tb0BfKZ6CzMa%2FYCbxw5sVQQks%2F%2BEhWi0%2B29s7PdXcQGYxJGChT%2FRysDxAZWmhSQRT3D6WT90GqkwJJA%3D%3D';
+const busStopNumberUri = `http://ws.bus.go.kr/api/rest/stationinfo/getStationByName?ServiceKey=${busStopKey}&stSrch=${busStopName}`;
+fetch(busStopNumberUri)
+    .then(res => res.text()) // XML
+    .then(xml => parseString(xml, (err, result) => {
+      if(err){
+        console.log(err);
+        return;
+      }
+      result.ServiceResult.msgBody[0].itemList.forEach(element => {
+        // console.log(element.arsId)
+        busStopNumbers.push(element.arsId);
+        
+      });
+    }))
+    .then(json => (busStopNumbers).forEach((element) => {
+      const busTime = `http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid?ServiceKey=${busStopKey}&arsId=${element}`;
+    }))
+    .catch(err => console.log(err));
+
+
+
+
+// const soyeon = {
+//   age: 25
+//   gender: 'female',
+// }
+
+// console.log(soyeon.age);
+
+// const { age } = soyeon;
+
+
+// .then(res => res.text())
+// .then(function(res){
+//   return res.text();
+// })
